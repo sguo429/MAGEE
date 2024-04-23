b1998ceb32cda89c8e3db2c8bac1fb2e94b075eb
Han Chen <han.chen.2@uth.tmc.edu>
2024-04-23 14:23:01 -0500

    v1.4.0
     Changes to be committed:
    	modified:   DESCRIPTION
    	modified:   R/glmm.gei.meta.R
    	modified:   README.md
    	modified:   inst/extdata/meta1.txt
    	modified:   inst/extdata/meta2.txt
    	modified:   inst/extdata/meta3.txt
    	modified:   inst/extdata/meta4.txt
    	modified:   inst/extdata/meta5.txt
    	modified:   inst/extdata/readme
    	modified:   man/MAGEE-package.Rd
    	modified:   vignettes/MAGEE.Rnw

--- R/glmm.gei.meta.R
+++ R/glmm.gei.meta.R
@@ -5,7 +5,7 @@ glmm.gei.meta <- function(files, outfile, interaction, SNPID = rep("SNPID", leng
   if(length(POS) != k) stop("Error: \"POS\" must have the same length as \"files\"!")
   if(length(Non_Effect_Allele) != k) stop("Error: \"Non_Effect_Allele\" must have the same length as \"files\"!")
   if(length(Effect_Allele) != k) stop("Error: \"Effect_Allele\" must have the same length as \"files\"!")
-  col.include <- c("Beta_Marginal", "SE_Beta_Marginal", "P_Value_Marginal", "Beta_G", paste0("Beta_G.",interaction), "SE_Beta_G", paste0("SE_Beta_G.", interaction), paste0("Cov_Beta_G_G.", interaction), "P_Value_Interaction", "P_Value_Joint")
+  col.include <- c("Beta_Marginal", "SE_Beta_Marginal", "P_Value_Marginal", "Beta_G", paste0("Beta_G-",interaction), "SE_Beta_G", paste0("SE_Beta_G-", interaction), paste0("Cov_Beta_G_G-", interaction), "P_Value_Interaction", "P_Value_Joint")
   master <- fread(files[1], header=T, data.table = FALSE)[, c(SNPID[1], CHR[1], POS[1],Non_Effect_Allele[1], Effect_Allele[1], "N_Samples", "AF", col.include)]
   names(master)[1:5] <- c("SNPID", "CHR", "POS", "Non_Effect_Allele", "Effect_Allele")
   master <- master[apply(!is.na(master[, col.include]), 1, all),]
@@ -13,13 +13,13 @@ glmm.gei.meta <- function(files, outfile, interaction, SNPID = rep("SNPID", leng
   master$SNPID2 <- paste(master$CHR, master$POS, master$Effect_Allele, master$Non_Effect_Allele, sep = ":")
   master$SCORE <- master$Beta_Marginal/master$SE_Beta_Marginal^2
   master$VAR <- 1/master$SE_Beta_Marginal^2
-  master$intSCORE <- master[,paste0("Beta_G.", interaction)]/master[,paste0("SE_Beta_G.", interaction)]^2
-  master$intVAR <- 1/master[,paste0("SE_Beta_G.", interaction)]^2
-  master$jointSCORE1 <- (master[,paste0("SE_Beta_G.", interaction)]^2*master$Beta_G-master[,paste0("Cov_Beta_G_G.", interaction)]*master[,paste0("Beta_G.", interaction)])/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G.", interaction)]^2-master[,paste0("Cov_Beta_G_G.", interaction)]^2)
-  master$jointSCORE2 <- (master$SE_Beta_G^2*master[,paste0("Beta_G.", interaction)]-master[,paste0("Cov_Beta_G_G.", interaction)]*master$Beta_G)/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G.", interaction)]^2-master[,paste0("Cov_Beta_G_G.", interaction)]^2)
-  master$jointVAR11 <- master[,paste0("SE_Beta_G.", interaction)]^2/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G.", interaction)]^2-master[,paste0("Cov_Beta_G_G.", interaction)]^2)
-  master$jointVAR12 <- -master[,paste0("Cov_Beta_G_G.", interaction)]/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G.", interaction)]^2-master[,paste0("Cov_Beta_G_G.", interaction)]^2)
-  master$jointVAR22 <- master$SE_Beta_G^2/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G.", interaction)]^2-master[,paste0("Cov_Beta_G_G.", interaction)]^2)
+  master$intSCORE <- master[,paste0("Beta_G-", interaction)]/master[,paste0("SE_Beta_G-", interaction)]^2
+  master$intVAR <- 1/master[,paste0("SE_Beta_G-", interaction)]^2
+  master$jointSCORE1 <- (master[,paste0("SE_Beta_G-", interaction)]^2*master$Beta_G-master[,paste0("Cov_Beta_G_G-", interaction)]*master[,paste0("Beta_G-", interaction)])/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G-", interaction)]^2-master[,paste0("Cov_Beta_G_G-", interaction)]^2)
+  master$jointSCORE2 <- (master$SE_Beta_G^2*master[,paste0("Beta_G-", interaction)]-master[,paste0("Cov_Beta_G_G-", interaction)]*master$Beta_G)/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G-", interaction)]^2-master[,paste0("Cov_Beta_G_G-", interaction)]^2)
+  master$jointVAR11 <- master[,paste0("SE_Beta_G-", interaction)]^2/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G-", interaction)]^2-master[,paste0("Cov_Beta_G_G-", interaction)]^2)
+  master$jointVAR12 <- -master[,paste0("Cov_Beta_G_G-", interaction)]/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G-", interaction)]^2-master[,paste0("Cov_Beta_G_G-", interaction)]^2)
+  master$jointVAR22 <- master$SE_Beta_G^2/(master$SE_Beta_G^2*master[,paste0("SE_Beta_G-", interaction)]^2-master[,paste0("Cov_Beta_G_G-", interaction)]^2)
   flag <- rep(0, nrow(master))
   if(k > 1) {
     for(i in 2:k) {
@@ -30,13 +30,13 @@ glmm.gei.meta <- function(files, outfile, interaction, SNPID = rep("SNPID", leng
       tmp$SNPID2 <- paste(tmp$CHR, tmp$POS, tmp$Effect_Allele, tmp$Non_Effect_Allele, sep = ":")
       tmp$SCORE <- tmp$Beta_Marginal/tmp$SE_Beta_Marginal^2
       tmp$VAR <- 1/tmp$SE_Beta_Marginal^2
-      tmp$intSCORE <- tmp[,paste0("Beta_G.", interaction)]/tmp[,paste0("SE_Beta_G.", interaction)]^2
-      tmp$intVAR <- 1/tmp[,paste0("SE_Beta_G.", interaction)]^2
-      tmp$jointSCORE1 <- (tmp[,paste0("SE_Beta_G.", interaction)]^2*tmp$Beta_G-tmp[,paste0("Cov_Beta_G_G.", interaction)]*tmp[,paste0("Beta_G.", interaction)])/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G.", interaction)]^2-tmp[,paste0("Cov_Beta_G_G.", interaction)]^2)
-      tmp$jointSCORE2 <- (tmp$SE_Beta_G^2*tmp[,paste0("Beta_G.", interaction)]-tmp[,paste0("Cov_Beta_G_G.", interaction)]*tmp$Beta_G)/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G.", interaction)]^2-tmp[,paste0("Cov_Beta_G_G.", interaction)]^2)
-      tmp$jointVAR11 <- tmp[,paste0("SE_Beta_G.", interaction)]^2/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G.", interaction)]^2-tmp[,paste0("Cov_Beta_G_G.", interaction)]^2)
-      tmp$jointVAR12 <- -tmp[,paste0("Cov_Beta_G_G.", interaction)]/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G.", interaction)]^2-tmp[,paste0("Cov_Beta_G_G.", interaction)]^2)
-      tmp$jointVAR22 <- tmp$SE_Beta_G^2/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G.", interaction)]^2-tmp[,paste0("Cov_Beta_G_G.", interaction)]^2)
+      tmp$intSCORE <- tmp[,paste0("Beta_G-", interaction)]/tmp[,paste0("SE_Beta_G-", interaction)]^2
+      tmp$intVAR <- 1/tmp[,paste0("SE_Beta_G-", interaction)]^2
+      tmp$jointSCORE1 <- (tmp[,paste0("SE_Beta_G-", interaction)]^2*tmp$Beta_G-tmp[,paste0("Cov_Beta_G_G-", interaction)]*tmp[,paste0("Beta_G-", interaction)])/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G-", interaction)]^2-tmp[,paste0("Cov_Beta_G_G-", interaction)]^2)
+      tmp$jointSCORE2 <- (tmp$SE_Beta_G^2*tmp[,paste0("Beta_G-", interaction)]-tmp[,paste0("Cov_Beta_G_G-", interaction)]*tmp$Beta_G)/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G-", interaction)]^2-tmp[,paste0("Cov_Beta_G_G-", interaction)]^2)
+      tmp$jointVAR11 <- tmp[,paste0("SE_Beta_G-", interaction)]^2/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G-", interaction)]^2-tmp[,paste0("Cov_Beta_G_G-", interaction)]^2)
+      tmp$jointVAR12 <- -tmp[,paste0("Cov_Beta_G_G-", interaction)]/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G-", interaction)]^2-tmp[,paste0("Cov_Beta_G_G-", interaction)]^2)
+      tmp$jointVAR22 <- tmp$SE_Beta_G^2/(tmp$SE_Beta_G^2*tmp[,paste0("SE_Beta_G-", interaction)]^2-tmp[,paste0("Cov_Beta_G_G-", interaction)]^2)
       idx <- tmp$SNPID %in% master$SNPID | tmp$SNPID2 %in% master$SNPID
       if(sum(!idx) > 0) {
         flag <- c(flag, rep(0, sum(!idx)))
@@ -74,10 +74,10 @@ glmm.gei.meta <- function(files, outfile, interaction, SNPID = rep("SNPID", leng
     master$P_Value_Marginal <- pchisq(master$SCORE^2/master$VAR, 1, lower.tail=F)
     master$P_Value_Interaction <- pchisq(master$intSCORE^2/master$intVAR, 1, lower.tail=F)
     master$SE_Beta_G <- sqrt(master$jointVAR22/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2))
-    master[,paste0("SE_Beta_G.", interaction)] <- sqrt(master$jointVAR11/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2))
-    master[,paste0("Cov_Beta_G_G.", interaction)] <- -master$jointVAR12/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2)
+    master[,paste0("SE_Beta_G-", interaction)] <- sqrt(master$jointVAR11/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2))
+    master[,paste0("Cov_Beta_G_G-", interaction)] <- -master$jointVAR12/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2)
     master$Beta_G <- (master$jointVAR22*master$jointSCORE1-master$jointVAR12*master$jointSCORE2)/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2)
-    master[,paste0("Beta_G.", interaction)] <- (master$jointVAR11*master$jointSCORE2-master$jointVAR12*master$jointSCORE1)/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2)
+    master[,paste0("Beta_G-", interaction)] <- (master$jointVAR11*master$jointSCORE2-master$jointVAR12*master$jointSCORE1)/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2)
     master$P_Value_Joint <- pchisq((master$jointVAR22*master$jointSCORE1^2-2*master$jointVAR12*master$jointSCORE1*master$jointSCORE2+master$jointVAR11*master$jointSCORE2^2)/(master$jointVAR11*master$jointVAR22-master$jointVAR12^2), 2, lower.tail=F)
   }
   master <- master[, c("SNPID", "CHR", "POS", "Non_Effect_Allele", "Effect_Allele", "N_Samples", "AF", col.include)]
